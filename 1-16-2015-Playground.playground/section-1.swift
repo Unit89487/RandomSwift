2b73761d9f3557d1f4cbb499f021c95f5d6cb8bc
Jay Abdallah <jay.h.abdallah@gmail.com>
2015-01-19 22:25:52 -0500

    Page 9

--- 1-16-2015-Playground.playground/section-1.swift
+++ 1-16-2015-Playground.playground/section-1.swift
@@ -39,7 +39,7 @@ let aDoub: Double = 10.5
 let intDoub = anInt + (Int)(aDoub)
 
 let aString = "Zippady doo da for "
-let anInt = 10
+let oneInt = 10
 let aUnitOfTime = "hours"
 let statement = aString + String(anInt) + aUnitOfTime
 
@@ -68,7 +68,7 @@ array[1] = "Free tickets"
 var occupations = ["Malcolm":"Captain", "Kaylee":"Mechanic", "Joss":"Director"]
 occupations["Jayne"] = "Public Relations"
 
-var moreOccupations = ["Wilhelm":"Attorney", "Leonhard: Professor", "Henri":"Public relations"]
+var moreOccupations = ["Wilhelm":"Attorney", "Leonhard": "Researcher", "Henri":"Public relations"]
 
 //Create an empty array or dictionary using initializer syntax 
 let emptyArray = [String]()
@@ -82,6 +82,7 @@ occupations = [:]
 let individualScores = [75, 43, 103, 87, 12]
 var teamScore = 0
 for score in individualScores {
+    
     if score > 50 {
         teamScore += 3
     } else {
@@ -100,6 +101,27 @@ var greeting = "Hello!"
 if let name = optionalName {
     greeting = "Hello, \(name)"
 }
+
+// Experiment 
+
+
+var anotherOptionalString: String? = "Howdy doo"
+anotherOptionalString == nil
+
+var anotherOptionalName: String? = "Jimbo Baggins"
+anotherOptionalName == nil
+if let name = anotherOptionalName {
+    greeting = "Howdy doo, \(name)"
+} else {
+    let other = "Chimbo"
+    greeting = "It's nil, \(other)"
+}
+
+
+
+
+
+
 
 
 
